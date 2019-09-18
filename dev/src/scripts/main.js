f4b67dd24620113d9870edd2d445b868d4ff3613
Caleb Burton <caleb.burton@covenanteyes.com>
2019-09-18 16:56:23 -0400

    (dev): Add CSS dev toggles

--- dev/src/scripts/main.js
+++ dev/src/scripts/main.js
@@ -8,6 +8,29 @@ function component() { // eslint-disable-line no-unused-vars
 
 function init() {
     document.getElementById('copyright-year').innerText = new Date().getFullYear();
+
+
+    const devToggles = {
+        bg: {
+            element: document.querySelector('.dev-toggle[name="bg"]'),
+            callback: function () {
+                document.querySelector('html').classList.toggle('dev-bg');
+            },
+        },
+        border: {
+            element: document.querySelector('.dev-toggle[name="border"]'),
+            callback: function () {
+                document.querySelector('html').classList.toggle('dev-border');
+            },
+        },
+    };
+    
+    for (const toggleName in devToggles) {
+        const toggle = devToggles[toggleName];
+        if (toggle.element) {
+            toggle.element.addEventListener('click', toggle.callback);
+        }
+    }
 }
 
 window.onload = function() {
