bc1ced69ff04b22261d0dda9830437abf4c62f83
Caleb Burton <caleb.burton@covenanteyes.com>
2020-10-08 16:03:25 -0400

    chore(lint): use single quotes in JS

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,13 +4,13 @@ function init() {
         bg: {
             element: document.querySelector('.dev-toggle[name="bg"]'),
             callback: function () {
-                document.querySelector("html").classList.toggle("dev-bg");
+                document.querySelector('html').classList.toggle('dev-bg');
             },
         },
         border: {
             element: document.querySelector('.dev-toggle[name="border"]'),
             callback: function () {
-                document.querySelector("html").classList.toggle("dev-border");
+                document.querySelector('html').classList.toggle('dev-border');
             },
         },
     };
@@ -18,19 +18,19 @@ function init() {
     for (const toggleName in devToggles) {
         const toggle = devToggles[toggleName];
         if (toggle.element) {
-            toggle.element.addEventListener("click", toggle.callback);
+            toggle.element.addEventListener('click', toggle.callback);
         }
     }
     // #endregion
 
     // #region *** Hamburger Menu
-    const navToggleElement = document.querySelector("header .nav-toggle");
-    const navMenuElement = document.querySelector("header nav");
+    const navToggleElement = document.querySelector('header .nav-toggle');
+    const navMenuElement = document.querySelector('header nav');
     if (navToggleElement && navMenuElement) {
-        navToggleElement.addEventListener("click", () => {
+        navToggleElement.addEventListener('click', () => {
             const navIsHidden =
-                navMenuElement.attributes["aria-hidden"] && navMenuElement.attributes["aria-hidden"].value === "true";
-            navMenuElement.setAttribute("aria-hidden", !navIsHidden);
+                navMenuElement.attributes['aria-hidden'] && navMenuElement.attributes['aria-hidden'].value === 'true';
+            navMenuElement.setAttribute('aria-hidden', !navIsHidden);
         });
     }
     // #endregion
