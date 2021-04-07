8089a03cb23470161314789646cb5c8ef3384220
Caleb Burton <ccburton4@gmail.com>
2021-04-07 11:00:38 -0400

    feat(): Contact section

--- gatsby/src/components/contact/index.js
+++ gatsby/src/components/contact/index.js
@@ -6,8 +6,15 @@ import Social from "../social";
 import styles from "./contact.module.scss";
 
 const Contact = () => (
-  <section className={styles.contactBackground}>
-    <div className={styles.contact}>
+  <section className={`${styles.contactBackground}`} id="contact">
+    <div className={`heading ${styles.contact}`}>
+      <h2 className={`text--xl font--heading font--bold ${styles.head}`}>
+        Get in touch!
+      </h2>
+      <p className={`text--md ${styles.subhead}`}>
+        Whether you're looking for development help, want to work together, or
+        have any queries at all, I'd love to hear from you.
+      </p>
       <ul className={styles.socials}>
         {socialData.map((social, index) => (
           <Social
