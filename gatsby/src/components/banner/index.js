8e6a53592a2fb7a7552eec781eeee9fe8617f794
Caleb Burton <ccburton4@gmail.com>
2021-04-07 11:09:49 -0400

    feat(banner): Add arrow and smooth scroll

--- gatsby/src/components/banner/index.js
+++ gatsby/src/components/banner/index.js
@@ -1,15 +1,43 @@
 import React from "react";
 import styles from "./banner.module.scss";
+import { Link } from "react-scroll";
 
 const Banner = () => {
   return (
-    <header className={styles.banner}>
-      <div className={styles.text}>
-        <h1 className="text--xxl">Caleb Burton</h1>
-        <div className={styles.line} />
-        <h2 className={`text--md ${styles.title}`}>Full Stack Developer</h2>
-      </div>
-    </header>
+    <>
+      <header className={styles.banner}>
+        <div className={styles.text}>
+          <h1 className="text--xxl">Caleb Burton</h1>
+          <div className={styles.line} />
+          <h2 className={`text--md ${styles.title}`}>Full Stack Developer</h2>
+        </div>
+      </header>
+      <Link
+        className={styles.arrow}
+        to={"about"}
+        spy={true}
+        smooth={true}
+        offset={-10}
+        duration={1000}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          width="44"
+          height="44"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M19 9l-7 7-7-7"
+          />
+        </svg>
+      </Link>
+    </>
   );
 };
 
