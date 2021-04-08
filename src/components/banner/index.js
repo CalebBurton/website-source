8e65c242fe73c7e76050cf360d1a8b485fe23681
Caleb Burton <ccburton4@gmail.com>
2021-04-08 08:25:15 -0400

    feat(): Text update

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -7,7 +7,7 @@ const Banner = () => {
     <>
       <header className={styles.banner}>
         <div className={styles.text}>
-          <h1 className="text--xxl">Caleb Charles Burton</h1>
+          <h1 className="text--xxl">Caleb C. Burton</h1>
           <div className={styles.line} />
           <h2 className={`text--md ${styles.title}`}>Full Stack Developer</h2>
         </div>
