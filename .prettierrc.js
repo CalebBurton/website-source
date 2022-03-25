023d3b21fca7db771ed3ce3faa3ce9625b8e298c
Caleb Burton <ccburton4@gmail.com>
2022-03-25 15:58:25 -0400

    chore(tooling): update lint rules

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // All of these are the defaults anyway, they're just included for clarity.
+  // See https://prettier.io/docs/en/options.html for a full list of options
+  printWidth: 80, // this is a guideline, NOT a hard limit
+  tabWidth: 2,
+  useTabs: false,
+  arrowParens: "always",
+  // Below are the customizations that are _different_ from the defaults.
+  // N/A
+};
